43dfcc1f6c210e354ab91e6f086a7af8ef0b1d6c
adamwjo <adamwjo@gmail.com>
2019-03-21 10:17:25 -0400

    basic function'

--- Contents.swift
+++ Contents.swift
@@ -1,3 +1,16 @@
 import UIKit
 
 var str = "Hello, playground"
+
+func thisIsaFunction() {
+    //this is a function in Swift.
+    //a function is declared with the "func" keyword.
+}
+
+func getMilk(){
+    print("Got milk")
+}
+
+getMilk()
+
+
